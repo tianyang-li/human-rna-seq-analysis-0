02c080fd80e0449bc6b90ed6f5a836a6adc2461d
Tianyang Li <tmy1018@gmail.com>
2012-09-04 12:33:38 +0800

    see how many branches there are at each isoform's end

--- isoform_stats_0.py
+++ isoform_stats_0.py
@@ -40,31 +40,8 @@ def main():
     
     for chr_name, chr_gl in gene_loci.iteritems():
         for gl in chr_gl:
-            chain_tot = len(gl.ex_chains)
             for exs in gl.t_exs.itervalues():
-                chain_set = set([])
-                for ex in exs:
-                    chain_set.add(gl.ex2chain[ex])
-                left_no_distinct = False
-                right_no_distinct = False
-                if exs[0].start != gl.ex2chain[exs[0]].exons[0].start:
-                    left_no_distinct = True
-                if exs[-1].end != gl.ex2chain[exs[-1]].exons[-1].end:
-                    right_no_distinct = True
-                print chain_tot, len(chain_set),
-                if left_no_distinct:
-                    print 1,
-                else:
-                    print 0,
-                if right_no_distinct:
-                    print 1,
-                else:
-                    print 0,
-                if left_no_distinct and right_no_distinct:
-                    print 1,
-                else:
-                    print 0,
-                print
+                print len(exs[0].left_exons), len(exs[-1].right_exons)
 
 if __name__ == '__main__':
     main()
